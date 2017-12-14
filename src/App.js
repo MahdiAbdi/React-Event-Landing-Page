e081098ab09f926629876124059379d9b28fab7b
Mahdi <mahdiabdif@gmail.com>
2017-12-14 15:28:12 +0330

    initalizing base component and style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
   render() {
     return (
       <div>
