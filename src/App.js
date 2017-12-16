a515351521864b5644f85d5cb5188e8cb27c2b98
Mahdi <mahdiabdif@gmail.com>
2017-12-16 16:33:11 +0330

    About Component : Done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Home from './Components/Home/Home'
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+
 import './Styles/main.css'
 class App extends Component {
   constructor(props) {
@@ -7,13 +9,44 @@ class App extends Component {
     this.state = {
       home: {
         background: 'http://eazzy.me/html/imevent/assets/img/preview/background-img-slider.jpg'
+      },
+      about: {
+        icon: 'star',
+        title: "What's about event",
+        subtitle: "whats going on there come and learn",
+        text: "Praesent ac sem in neque venenatis tristique. Morbi et ligula velit. Nullam a augue vel mi porta vestibulum non ac elit. Vivamus convallis tortor et fermentum semper. In hac habitasse platea dictumst. Curabitur eget dui id metus pulvinar suscipit. Quisque vitae ligula laoreet, scelerisque leo quis, facilisis metus. Sed pellentesque, urna sed varius consectetur, eros augue fringilla magna, id sem magna vel diam. Nulla sed hendrerit nunc.",
+        buttons: [
+          {
+            text: 'Register',
+            icon: 'arrow-circle-right'
+          },
+          {
+            text: 'Watch Video',
+            icon: ''
+          }
+        ],
+        images: [
+          {
+            src: "http://eazzy.me/html/imevent/assets/img/preview/about-lg-1.jpg"
+          },
+          {
+            src: "http://eazzy.me/html/imevent/assets/img/preview/about-lg-2.jpg"
+          },
+          {
+            src: "http://eazzy.me/html/imevent/assets/img/preview/about-lg-3.jpg"
+          },
+          {
+            src: "http://eazzy.me/html/imevent/assets/img/preview/about-lg-4.jpg"
+          },
+        ]
       }
     }
   }
   render() {
     return (
       <div>
-        <Home background={this.state.home.background} />
+        {/* <Home background={this.state.home.background} /> */}
+        <About icon={this.state.about.icon} title={this.state.about.title} subtitle={this.state.about.subtitle} text={this.state.about.text} buttons={this.state.about.buttons} images={this.state.about.images} />
       </div>
     );
   }
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
 
 class About extends Component {
   constructor(props) {
@@ -6,8 +7,87 @@ class About extends Component {
   }
   render(){
     return(
-      <section>
-
+      <section className="container">
+        <h1 className="section-title">
+          <span>
+            <span className="fa-stack">
+              <i className="fa rhex fa-stack-2x"></i>
+              <i className="fa fa-star fa-stack-1x"></i>
+            </span>
+          </span>
+          <span className="title-inner animated fadeInRight visible">
+            {this.props.title} <small>/ {this.props.subtitle}</small>
+          </span>
+        </h1>
+        <Row>
+          <Col md="8">
+            <p className="animated fadeInUp visible">{this.props.text}</p>
+            <p className="btn-row">
+              <a href="#register" className="btn btn-theme btn-theme-xl scroll-to animated flipInY visible" >{this.props.buttons[0].text} <i className="fa fa-arrow-circle-right"></i></a>
+              <a href="#" className="btn btn-theme btn-theme-xl btn-theme-transparent animated flipInY visible" >{this.props.buttons[1].text}</a>
+            </p>
+          </Col>
+          <Col md="4">
+            <Row className="thumbnails">
+              <Col sm="6" xs="6">
+                <div className="thumbnail no-border no-padding animated fadeInLeft visible">
+                  <div className="media">
+                    <img src="http://eazzy.me/html/imevent/assets/img/preview/about-sm-4.jpg" alt="" />
+                    <div className="caption hovered">
+                      <div className="caption-wrapper div-table">
+                        <div className="caption-inner div-cell">
+                          <p className="caption-buttons"><a href="http://eazzy.me/html/imevent/assets/img/preview/about-lg-1.jpg" className="btn caption-zoom" data-gal="prettyPhoto"><i className="fa fa-search"></i></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col sm="6" xs="6">
+                <div className="thumbnail no-border no-padding animated fadeInLeft visible">
+                  <div className="media">
+                    <img src="http://eazzy.me/html/imevent/assets/img/preview/about-sm-1.jpg" alt="" />
+                    <div className="caption hovered">
+                      <div className="caption-wrapper div-table">
+                        <div className="caption-inner div-cell">
+                          <p className="caption-buttons"><a href="http://eazzy.me/html/imevent/assets/img/preview/about-lg-1.jpg" className="btn caption-zoom" data-gal="prettyPhoto"><i className="fa fa-search"></i></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col sm="6" xs="6">
+                <div className="thumbnail no-border no-padding animated fadeInLeft visible">
+                  <div className="media">
+                    <img src="http://eazzy.me/html/imevent/assets/img/preview/about-sm-2.jpg" alt="" />
+                    <div className="caption hovered">
+                      <div className="caption-wrapper div-table">
+                        <div className="caption-inner div-cell">
+                          <p className="caption-buttons"><a href="http://eazzy.me/html/imevent/assets/img/preview/about-lg-1.jpg" className="btn caption-zoom" data-gal="prettyPhoto"><i className="fa fa-search"></i></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+              <Col sm="6" xs="6">
+                <div className="thumbnail no-border no-padding animated fadeInLeft visible">
+                  <div className="media">
+                    <img src="http://eazzy.me/html/imevent/assets/img/preview/about-sm-3.jpg" alt="" />
+                    <div className="caption hovered">
+                      <div className="caption-wrapper div-table">
+                        <div className="caption-inner div-cell">
+                          <p className="caption-buttons"><a href="http://eazzy.me/html/imevent/assets/img/preview/about-lg-1.jpg" className="btn caption-zoom" data-gal="prettyPhoto"><i className="fa fa-search"></i></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </section>
     )
   }
